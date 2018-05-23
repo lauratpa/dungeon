0f6d06e9fb604a3fe71636b024bc005c8967f006
Laura Paakkinen <laura.paakkinen@u2i.com>
2018-05-23 19:50:15 +0200

    Don't let user step on things

--- systems/movement_system.rb
+++ systems/movement_system.rb
@@ -1,6 +1,6 @@
 class MovementSystem < System
   def initialize
-    super(component_types: [Position, Roomable, PlayerMovable])
+    super(component_types: [Position, Roomable])
   end
 
   private
@@ -9,28 +9,39 @@ class MovementSystem < System
     movement = Movement.call(player_input: player_input)
     return unless movement
 
-    entities.each do |entity|
-      old_position = entity.position
-      room = entity.roomable
-
-      attributes = old_position.attributes
-
-      coordinate = movement.fetch(:coordinate)
-      value = movement.fetch(:value)
-      attributes[coordinate] = attributes[coordinate] + value
-
-      new_position = Position.new(attributes)
-
-      return unless valid_move?(position: new_position, room: room)
-
-      entity.replace_component(
-        old_component: old_position,
-        new_component: new_position
-      )
+    player = entities.detect do |entity|
+      entity.components.map(&:type).to_set.superset?([PlayerMovable].to_set)
     end
+    return unless player
+
+    old_position = player.position
+    room = player.roomable
+
+    attributes = old_position.attributes
+
+    coordinate = movement.fetch(:coordinate)
+    value = movement.fetch(:value)
+    attributes[coordinate] = attributes[coordinate] + value
+
+    new_position = Position.new(attributes)
+    occupied_positions = entities
+      .tap { |ents| ents.delete(player) }
+      .map { |ents| ents.position }
+
+    return unless position_within_room?(position: new_position, room: room)
+    return if hits_an_object?(position: new_position, occupied_positions: occupied_positions)
+
+    player.replace_component(
+      old_component: old_position,
+      new_component: new_position
+    )
   end
 
-  def valid_move?(position:, room:)
+  def hits_an_object?(position:, occupied_positions:)
+    occupied_positions.any? { |o| o.attributes == position.attributes }
+  end
+
+  def position_within_room?(position:, room:)
     (room.min_x <= position.x && position.x < room.max_x) &&
       (position.y >= room.min_y && room.max_y > position.y)
   end
